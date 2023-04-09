1978ecc04f80955d2fbe53ca49ca7741d9d22b68
Pashka <8polrud8@gmail.com>
2023-04-09 12:34:10 +0200

    bug fixes in utc

--- ai_relise.py
+++ ai_relise.py
@@ -7,6 +7,7 @@ from apscheduler.schedulers.background import BackgroundScheduler
 
 bot = Bot('6024265589:AAEAsVOB-0w-IaeoS3Ach9bZxLxlg9U7MOo')
 dp = Dispatcher(bot)
+#  ============= Узнаём время на сегодня, завтра и вчера =============
 def get_now():
     return datetime.datetime.now()
 def get_today():
@@ -20,38 +21,51 @@ scheduler = BackgroundScheduler()
 moon_today = ''
 moon_yesterday = ''
 moon_tomorrow = ''
+content_today = ''
+content_yesterday = ''
+content_tomorrow = ''
 
+#======== Вытягиваем значения лунного дня ============
 def MoonDay(data_url):
-    global moon_today, moon_yesterday, moon_tomorrow
+    global moon_today, moon_yesterday, moon_tomorrow, content_today, content_yesterday, content_tomorrow
     url = f"https://www.mingli.ru/{data_url.strftime('%d-%m-%Y')}"
     soup = BeautifulSoup(requests.get(url).text, 'html.parser')
-    try:  # Вытягиваем значения для Лунного дня
-         moon = soup.find('div', class_='Content').find('div', class_='firstInfo').find('div', class_='MoonDay') \
+    try:# Вытягиваем значения для Лунного дня
+        moon = soup.find('div', class_='Content').find('div', class_='firstInfo').find('div', class_='MoonDay') \
                        .find_all(string=lambda text: isinstance(text, Comment))[1].split('DNone">')[1][:-13]
     except:
         moon = ''
+    try:# Вытягиваем на три дня со звёздами
+        content = soup.find('div', class_='Content').find('div', class_='firstInfo')
+    except:
+        content = ''
     # записываем данные в нужную переменную
     if data_url == get_today():
         moon_today = moon
+        content_today = content
     elif data_url == get_yesterday():
         moon_yesterday = moon
+        content_yesterday = content
     elif data_url == get_tomorrow():
         moon_tomorrow = moon
+        content_tomorrow = content
+    print(f'Выгрузка данных прошла успешно {get_now()}')
     return moon
 
-scheduler.add_job(MoonDay, 'cron', hour=6, minute=53, second=20, args=[get_today()])
-scheduler.add_job(MoonDay, 'cron', hour=6, minute=53, second=30, args=[get_yesterday()])
-scheduler.add_job(MoonDay, 'cron', hour=6, minute=53, second=40, args=[get_tomorrow()])
+#======== Обработка Шедулеров ============
+scheduler.add_job(MoonDay, 'cron', hour=7, minute=0, second=20, args=[get_today()])
+scheduler.add_job(MoonDay, 'cron', hour=7, minute=0, second=30, args=[get_yesterday()])
+scheduler.add_job(MoonDay, 'cron', hour=7, minute=0, second=40, args=[get_tomorrow()])
 scheduler.start()
 
 @dp.message_handler(commands=['start'])
 async def main(message):
     name = message.from_user.first_name
     nameid = message.from_user.id
-    conn = sqlite3.connect('testdata.sql')                      # strana INTEGER
+    conn = sqlite3.connect('testdata.sql')                      # utc INTEGER
     cur = conn.cursor()
     cur.execute('CREATE TABLE IF NOT EXISTS users (id int auto_increment primary key, name varchar(50), '
-                'pass varchar(50), strana integer)')
+                'pass varchar(50), utc integer)')
     cur.execute("SELECT * FROM users WHERE name=? AND pass=?", (name, nameid))
     existing_record = cur.fetchone()
     if existing_record:
@@ -83,8 +97,8 @@ async def maintest(message):
     cur.execute("SELECT * FROM users WHERE pass=?", (nameid,))
     existing_record = cur.fetchone()
     if existing_record:
-        strana = existing_record[-1]
-        timedelta = usertime + datetime.timedelta(hours=strana)
+        utc = existing_record[-1]
+        timedelta = usertime + datetime.timedelta(hours=utc)
     cur.close()
     conn.close()
     await message.answer(               f'Привет, <b>{name}.</b> '
@@ -103,11 +117,12 @@ async def allusers(message):
     cur.execute('SELECT * FROM users')
     users = cur.fetchall()
     info = ''
-    for el in users: info += f'Name: {el[1]}, ID:{el[2]}, Strana:{el[3]}\n'
+    for el in users: info += f'Name: {el[1]}, ID:{el[2]}, utc:{el[3]}\n'
     cur.close()
     conn.close()
     await message.answer(info)
-    #await bot.send_message(chat_id=237863350, text=info)
+    # await bot.send_message(chat_id=237863350, text=info)
+    # await bot.send_message(chat_id=678537666, text='Привет Татьяна. Как твои дела?')
 @dp.message_handler(commands=['time'])
 async def send_time(message):
     usertime = message.date
@@ -117,13 +132,19 @@ async def send_time(message):
     cur.execute("SELECT * FROM users WHERE pass=?", (nameid,))
     existing_record = cur.fetchone()
     if existing_record:
-        strana = existing_record[-1]
-        timedelta = usertime + datetime.timedelta(hours=strana)
+        utc = existing_record[-1]
+        timedelta = usertime + datetime.timedelta(hours=utc)
     cur.close()
     conn.close()
-    if timedelta.strftime('%d-%m-%Y') == get_today().strftime('%d-%m-%Y'): test = moon_today  #test = MoonDay(get_today())
-    if timedelta.strftime('%d-%m-%Y') == get_tomorrow().strftime('%d-%m-%Y'): test = moon_tomorrow  #test = MoonDay(get_tomorrow())
-    if timedelta.strftime('%d-%m-%Y') == get_yesterday().strftime('%d-%m-%Y'): test = moon_yesterday  #test = MoonDay(get_yesterday())
+    if timedelta.strftime('%d-%m-%Y') == get_today().strftime('%d-%m-%Y'): testmoon = moon_today  #test = MoonDay(get_today())
+    if timedelta.strftime('%d-%m-%Y') == get_tomorrow().strftime('%d-%m-%Y'): testmoon = moon_tomorrow  #test = MoonDay(get_tomorrow())
+    if timedelta.strftime('%d-%m-%Y') == get_yesterday().strftime('%d-%m-%Y'): testmoon = moon_yesterday  #test = MoonDay(get_yesterday())
+    if timedelta.strftime('%d-%m-%Y') == get_today().strftime('%d-%m-%Y'): testcontent = content_today  #test = MoonDay(get_today())
+    if timedelta.strftime('%d-%m-%Y') == get_tomorrow().strftime('%d-%m-%Y'): testcontent = content_tomorrow  #test = MoonDay(get_tomorrow())
+    if timedelta.strftime('%d-%m-%Y') == get_yesterday().strftime('%d-%m-%Y'): testcontent = content_yesterday  #test = MoonDay(get_yesterday())
+
+    DSymbol = testcontent.find('h5', class_='CzjanChu').text
+    DSymbolo = testcontent.find('p', class_='CzjanChu').text
     # print(timedelta.strftime('%d-%m-%Y'))
     # print(get_today().strftime('%d-%m-%Y'))
     # print(get_tomorrow().strftime('%d-%m-%Y'))
@@ -133,10 +154,13 @@ async def send_time(message):
     # print(test)
     current_time = datetime.datetime.now() # получаем текущую дату и время и форматируем ее в строку
     await message.answer(f"Время сервера : {current_time.strftime('%d-%m-%Y  %H:%M')}"
-                         f"\n================================"
+                         f"\n================"
                          f"\nВаше время  : {timedelta.strftime('%d-%m-%Y  %H:%M')}"
-                         f"\n================================"
-                         f"\nToday :  {test}"
+                         f"\n================"
+                         f"\nToday :  {testmoon}"
+                         f"\n================"
+                         f"\nToday :  {DSymbol}"
+                         f"\nToday :  {DSymbolo}"
                          )
 @dp.message_handler(commands=['moon'])
 async def Moon(message: types.message):
@@ -149,7 +173,7 @@ async def Moon(message: types.message):
 
 @dp.callback_query_handler()
 async def callback(call):
-    global moon_today, moon_yesterday, moon_tomorrow
+    global moon_today, moon_yesterday, moon_tomorrow, content_today, content_yesterday, content_tomorrow
     markup = types.InlineKeyboardMarkup()
     btn1 = types.InlineKeyboardButton('Yesterday', callback_data='yesterday')
     btn2 = types.InlineKeyboardButton('Today', callback_data='today')
@@ -161,6 +185,9 @@ async def callback(call):
         await bot.edit_message_text(text=f'{get_yesterday().strftime("%d-%m-%Y")}'
                                          f'\n-----------'
                                          f'\n{moon_yesterday}'
+                                         f'\n-----------'
+                                         f"\n{content_yesterday.find('h5', class_='CzjanChu').text}"
+                                         f"\n{content_yesterday.find('p', class_='CzjanChu').text}"
                                     , chat_id=call.message.chat.id, message_id=call.message.message_id,
                                     reply_markup=markup)
     elif call.data == 'today':
@@ -169,6 +196,9 @@ async def callback(call):
         await bot.edit_message_text(text=f'{get_today().strftime("%d-%m-%Y")}'
                                          f'\n-----------'
                                          f'\n{moon_today}'
+                                         f'\n-----------'
+                                         f"\n{content_today.find('h5', class_='CzjanChu').text}"
+                                         f"\n{content_today.find('p', class_='CzjanChu').text}"
                                     ,chat_id=call.message.chat.id, message_id=call.message.message_id,  reply_markup=markup)
     elif call.data == 'tomorrow':
         if not moon_tomorrow:
@@ -176,6 +206,9 @@ async def callback(call):
         await bot.edit_message_text(text=f'{get_tomorrow().strftime("%d-%m-%Y")}'
                                          f'\n-----------'
                                          f'\n{moon_tomorrow}'
+                                         f'\n-----------'
+                                         f"\n{content_tomorrow.find('h5', class_='CzjanChu').text}"
+                                         f"\n{content_tomorrow.find('p', class_='CzjanChu').text}"
                                     , chat_id=call.message.chat.id, message_id=call.message.message_id,
                                     reply_markup=markup)
     elif call.data == 'Ukr':
@@ -187,12 +220,15 @@ async def callback(call):
         existing_record = cur.fetchone()
         if existing_record:
             # Обновляем запись в базе данных
-            cur.execute("UPDATE users SET strana = ? WHERE pass = ?", (1, nameid))
+            cur.execute("UPDATE users SET utc = ? WHERE pass = ?", (1, nameid))
             conn.commit()
-            cur.close()
-            conn.close()
-            strana = existing_record[-1]
-            timedelta = usertime + datetime.timedelta(hours=strana)
+            # Получаем обновленную запись из базы данных
+            cur.execute("SELECT utc FROM users WHERE pass=?", (nameid,))
+            existing_record = cur.fetchone()
+        utc = existing_record[-1]
+        timedelta = usertime + datetime.timedelta(hours=utc)
+        cur.close()
+        conn.close()
         await bot.send_message(call.message.chat.id, f'Вы выбрали Украина UTC+2\n{timedelta}')
 
     elif call.data == 'Pol':
@@ -204,12 +240,15 @@ async def callback(call):
         existing_record = cur.fetchone()
         if existing_record:
             # Обновляем запись в базе данных
-            cur.execute("UPDATE users SET strana = ? WHERE pass = ?", (0, nameid))
+            cur.execute("UPDATE users SET utc = ? WHERE pass = ?", (0, nameid))
             conn.commit()
-            cur.close()
-            conn.close()
-            strana = existing_record[-1]
-            timedelta = usertime + datetime.timedelta(hours=strana)
+            # Получаем обновленную запись из базы данных
+            cur.execute("SELECT utc FROM users WHERE pass=?", (nameid,))
+            existing_record = cur.fetchone()
+        utc = existing_record[-1]
+        timedelta = usertime + datetime.timedelta(hours=utc)
+        cur.close()
+        conn.close()
         await bot.send_message(call.message.chat.id, f'Вы выбрали Польша UTC+1\n{timedelta}')
 
     elif call.data == 'Usa':
@@ -221,12 +260,15 @@ async def callback(call):
         existing_record = cur.fetchone()
         if existing_record:
             # Обновляем запись в базе данных
-            cur.execute("UPDATE users SET strana = ? WHERE pass = ?", (-6, nameid))
+            cur.execute("UPDATE users SET utc = ? WHERE pass = ?", (-6, nameid))
             conn.commit()
-            cur.close()
-            conn.close()
-            strana = existing_record[-1]
-            timedelta = usertime + datetime.timedelta(hours=strana)
+            # Получаем обновленную запись из базы данных
+            cur.execute("SELECT utc FROM users WHERE pass=?", (nameid,))
+            existing_record = cur.fetchone()
+        utc = existing_record[-1]
+        timedelta = usertime + datetime.timedelta(hours=utc)
+        cur.close()
+        conn.close()
         await bot.send_message(call.message.chat.id, f'Вы выбрали США UTC-4\n{timedelta}')
 
 
